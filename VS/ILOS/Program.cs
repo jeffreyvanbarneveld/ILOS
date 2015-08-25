38dfa21513d59d14ecb72a430807b8e91d74ea46
NielsDev <niels@niels-mats.be>
2015-08-25 15:38:13 +0200

    Asm test

--- VS/ILOS/Program.cs
+++ VS/ILOS/Program.cs
@@ -4,11 +4,25 @@ namespace ILOS
 {
     class Program
     {
+        static void Out8(ushort port, byte value)
+        {
+            Registers regs = default(Registers);
+            regs.eax = value;
+            regs.edx = port;
+            Asm.Execute("outb dx, al", regs);
+        }
+
         static void Main(string[] args)
         {
-            Registers regs = new Registers();
-            Asm.Execute("mov eax, 1337", regs);
-            Console.Write("eax=" + regs.eax);
+            int x = 5;
+            int y = 5;
+            int index = y * 80 + x;
+
+            // Move vga cursor as a test
+            Out8(0x3D4, 14);
+            Out8(0x3D5, (byte)(index >> 8));
+            Out8(0x3D4, 15);
+            Out8(0x3D5, (byte)(index & 0xFF));
         }
     }
 }
