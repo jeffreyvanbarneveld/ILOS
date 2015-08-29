5c984fdac70537e3ab9fdd3717519c7d98d829c1
Jeffrey van Barneveld <jeffrey@jeffdev.nl>
2015-08-29 22:01:27 +0200

    Fixed comments in PCSpeaker.cs and portio

--- VS/ILOS/Drivers/Sound/PCSpeaker.cs
+++ VS/ILOS/Drivers/Sound/PCSpeaker.cs
@@ -11,28 +11,34 @@ namespace ILOS.Drivers.Sound
         /// <summary>
         /// Play sound through the PC speaker
         /// </summary>
-        /// <param name="frequence">PC speaker</param>
+        /// <param name="frequence">Frequence</param>
         public static void Play(uint frequence)
         {
-            uint div;
-            byte temp;
+            // Calculate countdown value
+            uint countdown = 1193180 / frequence;
 
-            div = 1193180 / frequence;
-
+            // Set PIT to the frequency
             Portio.Out8(0x43, 0xB6);
-            Portio.Out8(0x42, (byte)(div));
-            Portio.Out8(0x42, (byte)(div >> 8));
+            Portio.Out8(0x42, (byte)(countdown));
+            Portio.Out8(0x42, (byte)(countdown >> 8));
 
-            temp = Portio.In8(0x61);
+            // Play the sound using the PC speaker
+            byte temp = Portio.In8(0x61);
+
+            // Is the speaker on?
             if (temp != (temp | 3))
+            {
+                // Set first two bits to 1 to turn on the speaker
                 Portio.Out8(0x61, (byte)(temp | 3));
+            }
         }
 
         /// <summary>
-        /// Stop sound from the pc speaker
+        /// Stop sound from the PC speaker
         /// </summary>
         public static void Stop()
         {
+            // Set first 2 bits to zero to stop the PC speaker
             byte temp = (byte)(Portio.In8(0x61) & 0xFC);
 
             Portio.Out8(0x61, temp);
--- VS/ILOS/Portio.cs
+++ VS/ILOS/Portio.cs
@@ -6,38 +6,59 @@ namespace ILOS
     {
         private static Registers regs = default(Registers);
 
+        /// <summary>
+        /// Write 8-bit value to the given port
+        /// </summary>
+        /// <param name="port">The port</param>
+        /// <param name="value">The value</param>
         public static void Out8(ushort port, byte value)
         {
-            //Registers regs = default(Registers);
             regs.eax = value;
             regs.edx = port;
             Asm.Execute("out dx, al", regs);
         }
 
+        /// <summary>
+        /// Read 8-bit value from the given port
+        /// </summary>
+        /// <param name="port">The port</param>
+        /// <returns>Value from the port</returns>
         public static byte In8(ushort port)
         {
-            //Registers regs = default(Registers);
             regs.edx = port;
             Asm.Execute("in al, dx", regs);
             return (byte)regs.eax;
         }
 
+        /// <summary>
+        /// Write 16-bit value to the given port
+        /// </summary>
+        /// <param name="port">The port</param>
+        /// <param name="value">The value</param>
         public static void Out16(ushort port, ushort value)
         {
-            //Registers regs = default(Registers);
             regs.eax = value;
             regs.edx = port;
             Asm.Execute("out dx, ax", regs);
         }
 
+        /// <summary>
+        /// Read 16-bit value from the given port
+        /// </summary>
+        /// <param name="port">The port</param>
+        /// <returns>Value from the port</returns>
         public static ushort In16(ushort port)
         {
-            //Registers regs = default(Registers);
             regs.edx = port;
             Asm.Execute("in ax, dx", regs);
             return (ushort)regs.eax;
         }
 
+        /// <summary>
+        /// Write 32-bit value to the given port
+        /// </summary>
+        /// <param name="port">The port</param>
+        /// <param name="value">The value</param>
         public static void Out32(ushort port, uint value)
         {
             regs.eax = value;
@@ -45,6 +66,11 @@ namespace ILOS
             Asm.Execute("out dx, eax", regs);
         }
 
+        /// <summary>
+        /// Read 32-bit value from the given port
+        /// </summary>
+        /// <param name="port">The port</param>
+        /// <returns>Value from the port</returns>
         public static uint In32(ushort port)
         {
             regs.edx = port;
